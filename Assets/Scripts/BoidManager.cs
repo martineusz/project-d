a029b918753f4228b88b4bba0839f55da1248a86
MateuszPiechowski <mateusz.piechowski@interia.pl>
2025-06-29 01:09:15 +0200

    added basic boids

---
+++ Assets/Scripts/BoidManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class BoidManager : MonoBehaviour
+{
+    public GameObject boidPrefab;
+    public int boidCount = 50;
+    public float spawnRadius = 10f;
+
+    [HideInInspector] public List<Boid> allBoids = new List<Boid>();
+
+    private void Start()
+    {
+        for (int i = 0; i < boidCount; i++)
+        {
+            Vector2 spawnPos = Random.insideUnitCircle * spawnRadius;
+            GameObject boidGO = Instantiate(boidPrefab, spawnPos, Quaternion.identity);
+            Boid boid = boidGO.GetComponent<Boid>();
+            boid.manager = this;
+            allBoids.Add(boid);
+        }
+    }
+}
