cc8a07a9572b8f686ce6292e9336a2c2a247a408
MateuszPiechowski <mateusz.piechowski@interia.pl>
2025-07-09 14:54:17 +0200

    treasure chests

---
+++ Assets/Scripts/Environment/Miscellaneous/TreasureChest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Items;
+using UI.Inventory;
+using UnityEngine;
+
+namespace Environment.Miscellaneous
+{
+    public class TreasureChest : MonoBehaviour, INteractive
+    {
+        public List<ItemData> treasureItems = new List<ItemData>();
+        
+        public void Use()
+        {
+            if (TryTakeResource(out var resource))
+            {
+                Inventory.Instance.Add(resource);
+                Debug.Log("Item taken from treasure and added to inventory.");
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private bool TryTakeResource(out ItemData resource)
+        {
+            if (treasureItems.Count > 0)
+            {
+                resource = treasureItems[0];
+                treasureItems.RemoveAt(0);
+                return true;
+            }
+
+            resource = null;
+            return false;
+        }
+    }
+}
