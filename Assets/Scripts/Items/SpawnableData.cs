cbe22fc93b82673a7c878762c683a49eaf5aa295
MateuszPiechowski <mateusz.piechowski@interia.pl>
2025-07-13 23:00:23 +0200

    added buyable units

--- Assets/Scripts/Items/SpawnableData.cs
+++ Assets/Scripts/Items/SpawnableData.cs
@@ -2,16 +2,21 @@
 
 namespace Items
 {
+    [CreateAssetMenu(fileName = "newSpawnable", menuName = "Items/Spawnables", order = 0)]
     public class SpawnableData : ItemData
     {
         public GameObject prefabToSpawn;
-        public Transform spawnPoint;
-        
+        public Vector3 spawnPosition;
+        public Quaternion spawnRotation = Quaternion.identity;
+        public float spawnRadius = 0f;
+
         public override void Use()
         {
-            if (prefabToSpawn != null && spawnPoint != null)
+            if (prefabToSpawn != null)
             {
-                Object.Instantiate(prefabToSpawn, spawnPoint.position, spawnPoint.rotation);
+                Vector3 randomOffset = Random.insideUnitSphere * spawnRadius;
+                Vector3 finalPosition = spawnPosition + randomOffset;
+                Object.Instantiate(prefabToSpawn, finalPosition, spawnRotation);
             }
         }
     }
