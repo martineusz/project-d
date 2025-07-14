fbbccdd0a4a11be8b00759748835dc81c1a41736
MateuszPiechowski <mateusz.piechowski@interia.pl>
2025-07-15 01:51:41 +0200

    light pushes monsters

--- Assets/Scripts/LightManipulation/PlayerTorch.cs
+++ Assets/Scripts/LightManipulation/PlayerTorch.cs
@@ -9,6 +9,7 @@ namespace LightManipulation
     {
         public Light2D torchLight;
         public Inventory inventory;
+        public CircleCollider2D torchTrigger;
 
         public float lightDiminishFrequency = 1f;
         public float lightDiminishSpeed = 0.001f;
@@ -17,11 +18,18 @@ namespace LightManipulation
         private float _outerRadius;
 
         private float _lastDiminishTime = -Mathf.Infinity;
+        
+        public float maxForce = 20f;
+        public float maxForceRadiusOffset = 5f;
+        private float _maxForceRadius;
 
         private void Start()
         {
             _innerRadius = torchLight.pointLightInnerRadius;
             _outerRadius = torchLight.pointLightOuterRadius;
+            
+            torchTrigger.radius = _innerRadius;
+            _maxForceRadius = _innerRadius + maxForceRadiusOffset;
 
             if (torchLight == null)
             {
@@ -52,6 +60,8 @@ namespace LightManipulation
                 lightModifier -= lightDiminishSpeed;
                 torchLight.pointLightInnerRadius = _innerRadius * lightModifier;
                 torchLight.pointLightOuterRadius = _outerRadius * lightModifier;
+                torchTrigger.radius = torchLight.pointLightInnerRadius;
+                _maxForceRadius = torchLight.pointLightInnerRadius + maxForceRadiusOffset;
 
                 _lastDiminishTime = Time.time;
             }
@@ -65,9 +75,27 @@ namespace LightManipulation
             lightModifier = 1f;
             torchLight.pointLightInnerRadius = _innerRadius;
             torchLight.pointLightOuterRadius = _outerRadius;
+            torchTrigger.radius = _innerRadius;
+            _maxForceRadius = _innerRadius + maxForceRadiusOffset;
             
             return true;
 
+        }
+
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Enemy"))
+            {
+                Rigidbody2D enemyRb = collision.attachedRigidbody;
+                if (enemyRb == null) return;
+                
+                Vector2 direction = (collision.transform.position - transform.position);
+                float distance = direction.magnitude;
+                Vector2 forceDirection = direction.normalized;
+
+                float forceAmount = Mathf.Clamp01(1 - (distance / _maxForceRadius)) * maxForce;
+                enemyRb.AddForce(forceDirection * forceAmount, ForceMode2D.Impulse);
+            }
         }
     }
 }
