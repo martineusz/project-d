d06a28a91aec4939b8d84a373bd2055d155395a3
MateuszPiechowski <mateusz.piechowski@interia.pl>
2025-07-18 23:53:31 +0200

    bombs

---
+++ Assets/Scripts/Environment/Buildable/Traps/LandmineTrap.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Environment.Buildable.Traps
+{
+    public class LandmineTrap : AbstractTrap
+    {
+        public TrapBombExplosion bombExplosion;
+        public bool isArmed = true;
+        public override void TrapEnter(Collider2D other)
+        {
+            if (other.isTrigger) return;
+            if (!isArmed) return;
+            
+            bombExplosion.StartBomb();
+            isArmed = false;
+        }
+
+        public override void TrapExit(Collider2D other)
+        {
+            //Nothing
+        }
+
+        public override void TrapStay(Collider2D other)
+        {
+            // Nothing
+        }
+    }
+}
---
+++ Assets/Scripts/Environment/Buildable/Traps/TrapBombExplosion.cs
@@ -0,0 +1,42 @@
+﻿using Units.Combat.Enemies;
+using UnityEngine;
+
+namespace Environment.Buildable.Traps
+{
+    public class TrapBombExplosion : MonoBehaviour
+    {
+        public float explosionDamage = 100f;
+        public float explosionRadius = 10f;
+        public LayerMask affectedLayers;
+        public float explosionDelay = 2f;
+
+        private bool _hasExploded = false;
+
+        public void StartBomb()
+        {
+            Invoke(nameof(Explode), explosionDelay);
+        }
+
+        private void Explode()
+        {
+            _hasExploded = true;
+
+            Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, explosionRadius, affectedLayers);
+
+            foreach (Collider2D col in hitColliders)
+            {
+                if (col.CompareTag("Enemy"))
+                {
+                    Debug.Log("Hit: " + col.name);
+                    var enemy = col.GetComponent<EnemyCombat>();
+                    if (enemy != null)
+                    {
+                        enemy.TakeDamage(explosionDamage);
+                    }
+                }
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
