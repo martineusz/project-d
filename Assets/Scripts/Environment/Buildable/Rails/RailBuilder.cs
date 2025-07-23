4054ce90b06b78b125c990703a6fdc9f5890d8a2
MateuszPiechowski <mateusz.piechowski@interia.pl>
2025-07-23 17:32:30 +0200

    buildable rail prototype

---
+++ Assets/Scripts/Environment/Buildable/Rails/RailBuilder.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Environment.Buildable.Rails
+{
+    public class RailBuilder : MonoBehaviour, INteractive
+    {
+        public GameObject railPrefab;
+        
+        public SpriteRenderer spriteRenderer;
+        private Color _originalColor;
+        private bool _isHighlighted;
+        
+        private void Awake()
+        {
+            _originalColor = spriteRenderer.color;
+        }
+        
+        public void Use()
+        {
+            railPrefab.SetActive(true);
+            gameObject.SetActive(false);
+        }
+
+        public void Highlight()
+        {
+            if (_isHighlighted) return;
+            spriteRenderer.color = Color.white;
+            _isHighlighted = true;
+        }
+
+        public void DisableHighlight()
+        {
+            if (!_isHighlighted) return;
+            spriteRenderer.color = _originalColor;
+            _isHighlighted = false;
+        }
+    }
+}
